7ed4a2e31be3f3f759baabeb76e49bcef4800bf5
nguyenquanghuy1999 <nguyenquanghuy1999>
2026-05-12 16:31:58 +0700

    fix(header): correct logo spacing

--- src/components/Header/Logo/Logo.tsx
+++ src/components/Header/Logo/Logo.tsx
@@ -2,11 +2,11 @@ import { IconLogo } from "../../Icons/IconLogo";
 
 function Logo() {
   return (
-    <div className="relative hover:cursor-pointer">
-      <div className="absolute">
+    <div className="relative h-25 hover:cursor-pointer">
+      <div className="absolute mt-2">
         <IconLogo fill="var(--color-primary)" />
       </div>
-      <div className="absolute transition duration-300 hover:-translate-1">
+      <div className="absolute mt-2 transition duration-300 hover:-translate-1">
         <IconLogo text="A" fill="var(--color-surface)" />
       </div>
     </div>
