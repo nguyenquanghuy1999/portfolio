718819c11849e4369c1e8e8c400ad640adb277b0
nguyenquanghuy1999 <nguyenquanghuy1999>
2026-05-14 17:38:04 +0700

    feat(header): add responsive base

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,20 @@
+import { IconMenu } from "../Icons/IconMenu";
 import Logo from "./Logo";
 import Nav from "./Nav";
 import ResumeLink from "./ResumeLink";
 
 function Header() {
   return (
-    <div className="bg-surface flex h-25 items-center justify-between px-12.5">
+    <div className="bg-surface tablet:px-10 desktop:px-12.5 flex h-25 items-center justify-between px-6.25">
       <Logo />
-      <div className="flex items-center">
+      <div className="desktop:flex hidden">
         <Nav />
         <ResumeLink />
       </div>
+      <div className="desktop:hidden block">
+        <IconMenu />
+      </div>
     </div>
-  );  
+  );
 }
 export default Header;
