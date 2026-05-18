763c95f99880c70e73c99102ede183266c1157ec
nguyenquanghuy1999 <nguyenquanghuy1999>
2026-05-18 15:14:43 +0700

    feat(nav): add staggered animation and refactor nav structure

--- src/components/Header/Nav/Nav.tsx
+++ src/components/Header/Nav/Nav.tsx
@@ -2,16 +2,20 @@ const navList = ["About", "Experience", "Work", "Contact"];
 
 function Nav() {
   return (
-    <div className="desktop:flex desktop:flex-row flex-col">
+    <ul className="desktop:flex desktop:flex-row flex-col text-center hover:cursor-pointer">
       {navList.map((navItem, index) => (
-        <ul key={index} className="text-center hover:cursor-pointer">
-          <li className="text-text hover:text-brand desktop:text-[14px] p-5 text-sm text-[19px]">
-            <span className="text-brand desktop:text-[13px] desktop:inline block text-[17px]">{`0${++index}. `}</span>
-            {navItem}
-          </li>
-        </ul>
+        <li
+          key={index}
+          className="text-text hover:text-brand desktop:text-[14px] animate-fadeInUp p-5 text-sm text-[19px] opacity-0"
+          style={{
+            animationDelay: `${index * 0.2}s`,
+          }}
+        >
+          <span className="text-brand desktop:text-[13px] desktop:inline block text-[17px]">{`0${++index}. `}</span>
+          {navItem}
+        </li>
       ))}
-    </div>
+    </ul>
   );
 }
 export default Nav;
